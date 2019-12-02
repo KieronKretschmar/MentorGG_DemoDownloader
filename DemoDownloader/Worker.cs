d052a98b7ea36a94564b7c4f056ff6034e92a45e
Arran <code@arran.nz>
2019-12-02 16:14:24 +0100

    Collect test urls

--- DemoDownloader/Worker.cs
+++ DemoDownloader/Worker.cs
@@ -27,8 +27,15 @@ namespace DemoDownloader
             while (!stoppingToken.IsCancellationRequested)
             {
                 logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                string fileUrl = "https://demos-europe-west2.faceit-cdn.net/csgo/418fc933-9f72-418e-815f-566c86f125e0.dem.gz";
-                await blobStreamer.StreamToBlobAsync(fileUrl);
+                string[] valid_test_urls = {
+                    "http://replay191.valve.net/730/003380559004810871024_1371365513.dem.bz2",
+                    "https://demos-asia-southeast1.faceit-cdn.net/csgo/76d083e1-9808-48e4-aaf0-9d1d49343b28.dem.gz",
+                    "http://replay186.valve.net/730/003380681898857595197_1318256891.dem.bz2",
+                    "https://demos-europe-west2.faceit-cdn.net/csgo/5bb0cbe3-4045-45dd-a7c1-064422e52762.dem.gz",
+                    "https://demos-europe-west2.faceit-cdn.net/csgo/2af4b0e3-084e-4dd9-90e0-72b4f342ccbb.dem.gz",
+                    "https://demos-europe-west2.faceit-cdn.net/csgo/418fc933-9f72-418e-815f-566c86f125e0.dem.gz",
+                };
+                await blobStreamer.StreamToBlobAsync(valid_test_urls[1]);
                 await Task.Delay(100000, stoppingToken);
             }
         }
